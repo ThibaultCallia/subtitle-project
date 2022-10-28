fe0e634ccb4b887a9d8dcc05e11867766f3092e3
Thibault Calliauw <calliauw.t@gmail.com>
2022-10-28 15:18:05 +0200

    end of day

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-import fs, { existsSync } from "fs";
-const folderToRead = process.argv[2];
-if (!fs.existsSync(folderToRead)) {
-  console.log("no such file");
-  process.exit();
+import fs, { existsSync, readlinkSync } from "fs";
+import rl from "readline-sync";
+
+while (true) {
+  const folderToRead = /*process.argv[2] || */ rl.question(
+    "Please provide folder \n(or enter STOP to quit\n)"
+  );
+  if (folderToRead == "STOP") {
+    process.exit();
+  }
+  if (!fs.existsSync(folderToRead)) {
+    console.log("no such file");
+    process.exit();
+  }
+
+  const folderContents = fs.readdirSync(folderToRead);
+  let allContent = "";
+
+  for (let i = 0; i < folderContents.length; i++) {
+    allContent += fs.readFileSync(folderToRead + "/" + folderContents[i]);
+  }
+  allContent = allContent
+    .replace(/[^(a-z)]/gi, " ")
+    .replace(/ +/g, " ")
+    .toLowerCase();
+
+  const arr = allContent.split(" ").filter((a) => a.length > 1);
+  const wordsOcc = arr.reduce(function (obj, word) {
+    obj[word] = obj[word] + 1 || 1;
+    return obj;
+  }, {});
+
+  const uniqueWords = Object.keys(wordsOcc);
+  uniqueWords.sort((a, b) => wordsOcc[b] - wordsOcc[a]);
+
+  console.log(uniqueWords);
 }
-
-const folderContents = fs.readdirSync(folderToRead);
-let allContent = "";
-
-for (let i = 0; i < folderContents.length; i++) {
-  allContent += fs.readFileSync(folderToRead + "/" + folderContents[i]);
-}
-allContent = allContent
-  .replace(/[^(a-z)]/gi, " ")
-  .replace(/ +/g, " ")
-  .toLowerCase();
-
-const arr = allContent.split(" ").filter((a) => a.length > 1);
-const wordsOcc = arr.reduce(function (obj, word) {
-  obj[word] = obj[word] + 1 || 1;
-  return obj;
-}, {});
-
-const uniqueWords = Object.keys(wordsOcc);
-uniqueWords.sort((a, b) => wordsOcc[b] - wordsOcc[a]);
-
-console.log(uniqueWords);
