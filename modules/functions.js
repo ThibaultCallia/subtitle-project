b465123c5631e22d1beb98741aeaf4e92129ec8f
Thibault Calliauw <calliauw.t@gmail.com>
2022-10-29 11:36:12 +0200

    first steps to OOP

--- modules/functions.js
+++ modules/functions.js
@@ -1,29 +1,3 @@
-import fs, { existsSync, readlinkSync } from "fs";
-import rl from "readline-sync";
-
-/*const folderToRead =
-  process.argv[2] ||
-  rl.question("Please provide folder \n(or enter STOP to quit\n)");
-
-  ---> This would first check for a third variable in terminal (node index ...)
-        If nothing provided, rl.question is chosen
-*/
-
-const folderToRead = rl.question(
-  "Please provide folder \n(or enter STOP to quit\n)"
-);
-// if (folderToRead == "STOP") {
-//   process.exit();
-// }
-if (!fs.existsSync(folderToRead)) {
-  console.log("no such file");
-  process.exit();
-}
-
-const arr = createArray(folderToRead);
-console.log(arr);
-// console.log(sortAll(arr));
-
 function createArray(folderToRead) {
   // folderContents = array or file names (strings)
   const folderContents = fs.readdirSync(folderToRead);
@@ -43,10 +17,6 @@ function createArray(folderToRead) {
 
   return allContent.split(" ").filter((a) => a.length > 0);
 }
-// readline question : what do you want to do?
-//    - Look for specific word
-//    - Sort all words
-//    -
 
 function sortAll(arr) {
   const wordsOcc = arr.reduce(function (obj, word) {
